39576aaaf0bd15815cc0e95e2f7fd76903afbba5
Dfingerhut <d.fingerhut38@gmail.com>
2025-01-11 21:50:21 -0700

    modified:   .flaskenv 	new file:   __pycache__/__init__.cpython-312.pyc 	new file:   __pycache__/app.cpython-312.pyc 	new file:   __pycache__/routes.cpython-312.pyc 	renamed:    __init__.py -> app.py 	modified:   static/css/main.css 	modified:   templates/layout.html 	new file:   templates/macros/nav.html

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
-from "Movie Website".routes import pages
+from routes import pages
 
 load_dotenv()
 
@@ -11,10 +11,8 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     app.config["MONGODB_URI"] = os.environ.get("MONGODB_URI")
-    app.config["SECRET_KEY"] = os.environ.get(
-        "SECRET_KEY", "pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw"
-    )
-    app.db = MongoClient(app.config["MONGODB_URI"]).get_default_database()
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+    #app.db = MongoClient(app.config["MONGODB_URI"]).get_default_database()
 
     app.register_blueprint(pages)
     return app
